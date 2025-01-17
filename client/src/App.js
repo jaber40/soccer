57367dd088c2edcbfdb99710123689275e66c5d0
Jason <bernerjason@gmail.com>
2025-01-17 18:30:47 -0500

    2: created DataTable.js, changed app.js, installed Axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // If you're using a CSS file
+import DataTable from './DataTable'; // Import the DataTable component
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My SQL Data App</h1>
+      <DataTable /> {/* Display the DataTable component */}
     </div>
   );
 }
 
 export default App;
+
