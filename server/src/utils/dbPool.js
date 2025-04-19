19c17c12b55bde7df810e57a80cdeed2a61d61f3
Jason <bernerjason@gmail.com>
2025-04-19 13:47:32 -0400

    81: Database moved to TiDB

--- server/src/utils/dbPool.js
+++ server/src/utils/dbPool.js
@@ -5,6 +5,10 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 4000,
+  ssl: {
+    rejectUnauthorized: true
+  }
 };
 
 let dbPool = null;
