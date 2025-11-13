3d4923e8c2ab4549a01b8181cc5c59b642444dc0
Jason <bernerjason@gmail.com>
2025-11-12 22:11:17 -0500

    152: Removed auth from server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 // src/server.js
 const express = require('express');
 const cors = require('cors');
-const basicAuth = require('express-basic-auth'); // added for auth
 const tournamentRoutes = require('./src/routes/tournamentRoutes');
 const countryRoutes = require('./src/routes/countryRoutes');
 const playerRoutes = require('./src/routes/playerRoutes');
@@ -11,22 +10,8 @@ require('dotenv').config();
 const app = express();
 const port = 5000;
 
-
-// 🔒 Conditional Basic Auth
-// Only enable if ENABLE_SITE_AUTH=true in environment variables
-if (process.env.ENABLE_SITE_AUTH === 'true') {
-  app.use(
-    basicAuth({
-      users: { [process.env.SITE_USER]: process.env.SITE_PASS },
-      challenge: true, // triggers browser login popup
-      unauthorizedResponse: 'Access denied',
-    })
-  );
-}
-
 // Enable CORS
 app.use(cors());
-
 
 // API routes
 app.use('/api/tournaments', tournamentRoutes);
