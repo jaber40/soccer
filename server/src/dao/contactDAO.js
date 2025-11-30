20f5c062611532682b95638f4d23c2c351c43f03
Jason <bernerjason@gmail.com>
2025-11-29 22:04:50 -0500

    179: Retries added for Contact form

--- server/src/dao/contactDAO.js
+++ server/src/dao/contactDAO.js
@@ -1,19 +1,25 @@
 // server/src/dao/contactDAO.js
 const getDBPool = require('../utils/dbPool');
 
-function saveContact(name, email, message) {
+async function saveContact(name, email, message, retries = 12, delay = 5000) {
   const pool = getDBPool();
 
-  return new Promise((resolve, reject) => {
-    const sql = `
-      INSERT INTO contact (name, email, message)
-      VALUES (?, ?, ?)
-    `;
-    pool.query(sql, [name, email, message], (err, result) => {
-      if (err) return reject(err);
-      resolve(result.insertId);
-    });
-  });
+  for (let i = 0; i < retries; i++) {
+    try {
+      return await new Promise((resolve, reject) => {
+        const sql = `INSERT INTO contact (name, email, message) VALUES (?, ?, ?)`;
+        pool.query(sql, [name, email, message], (err, result) => {
+          if (err) return reject(err);
+          resolve(result.insertId);
+        });
+      });
+    } catch (err) {
+      console.warn(`DB not ready for contact form, retrying in ${delay}ms... (${i + 1}/${retries})`);
+      await new Promise((res) => setTimeout(res, delay));
+    }
+  }
+
+  throw new Error('Unable to save contact after multiple retries');
 }
 
 module.exports = { saveContact };
