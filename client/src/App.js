4ded4a384bc0decf9b44cf460505272d03aab8da
Jason <bernerjason@gmail.com>
2026-01-18 11:16:53 -0500

    272: App.js overlay CSS for Contact form

--- client/src/App.js
+++ client/src/App.js
@@ -256,6 +256,26 @@ const selectPlayer = (playerId) => {
       >
         Contact
       </button>
+
+
+      {/* Overlay + ContactForm */}
+      {showForm && (
+        <div
+          style={{
+            position: "fixed",
+            inset: 0,
+            backgroundColor: ""#3a3a3a", // gray overlay
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 9998, // under the button
+          }}
+        >
+          <div>
+            <ContactForm />
+          </div>
+        </div>
+      )}
       
 
     </div>
