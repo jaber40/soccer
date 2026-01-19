20062bcb321d4aabaf6a2ba674d4f0f59ba7c2eb
Jason <bernerjason@gmail.com>
2026-01-18 19:29:43 -0500

    284: App.js added birthplace text, club location

--- client/src/App.js
+++ client/src/App.js
@@ -377,13 +377,13 @@ const selectPlayer = (playerId) => {
             */}
        <div className="player-details">
         <div>{selectedPlayerDetails.player_name}</div>
-        <div style={{ color: "lightgray" }}>Birthplace:</div>
+        <div style={{ color: "gray" }}>Birthplace:</div>
         <div>{selectedPlayerDetails.player_city_name}, {selectedPlayerDetails.country_name}</div>
         <div>{selectedPlayerDetails.position}</div>
         <div>Age: {selectedPlayerDetails.age}</div>
         <div>{selectedPlayerDetails.club_name}</div>
-        <div style={{ color: "lightgray" }}>Club location:</div>
-        <div>{selectedPlayerDetails.club_city_name}</div>
+        <div style={{ color: "gray" }}>Club location:</div>
+        <div>{selectedPlayerDetails.club_city_name}, {selectedPlayerDetails.club_country_name}</div>
         <div>{selectedPlayerDetails.league_name}</div>
       </div>
 
